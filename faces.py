a7987e7b2ec737da1cb7bafb0bab24c29e66321c
Arafat4341 <mdyasinarafatyen2@gmail.com>
2019-02-25 12:24:17 -0800

    using face cascade

--- faces.py
+++ faces.py
@@ -7,6 +7,14 @@ cap = cv2.VideoCapture(0)
 
 while True:
 	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+	for(x,y,w,h) in faces:
+		roi_gray = gray[y:y+h, x:x+w]
+		img_item = 'myImg.png'
+
+		cv2.imwrite(img_item, roi_gray)
 
 	cv2.imshow('frame', frame)
 
@@ -15,4 +23,4 @@ while True:
 
 
 cap.release()
-cap.distroyAllWindows()
+cv2.destroyAllWindows()
